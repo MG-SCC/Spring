b44715b7d30a0bcf96e59f2b6fd259a1d0ace915
Matthew Gabrielle <mg244@myscc.ca>
2024-06-13 14:52:19 -0400

    Add project files.

---
+++ Controllers/AboutUs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Spring.Controllers;
+
+public class AboutUsController : Controller
+{
+    // 
+    // GET: /AboutUs/
+    public IActionResult Index()
+    {
+        return View();
+    }
+     
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+}
