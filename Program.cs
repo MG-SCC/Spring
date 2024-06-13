487ace01eeb2d2d530a8ddf65506d4797f262937
Matthew Gabrielle <mg244@myscc.ca>
2024-06-13 15:48:22 -0400

    Updated Program.cs with seed initializer

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Spring.Data;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SpringContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SpringContext") ?? throw new InvalidOperationException("Connection string 'SpringContext' not found.")));
+
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
