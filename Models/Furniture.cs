33326c4213ade4b8eaf5f1c4f9a1c10ffd41c3e2
Matthew Gabrielle <mg244@myscc.ca>
2024-06-13 15:58:53 -0400

    Seed Data

--- Models/Furniture.cs
+++ Models/Furniture.cs
@@ -6,8 +6,7 @@ public class Furniture
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    [DataType(DataType.Date)]
-    public DateTime Colour { get; set; }
+  
     public string? Type { get; set; }
     public decimal Price { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,42 +1,55 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using MvcMovie.Data;
-using MvcMovie.Models;
+using Spring.Data;
+using System;
+using System.Linq;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Spring.Models;
 
-builder.Services.AddDbContext<MvcMovieContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+public static class SeedData
 {
-    var services = scope.ServiceProvider;
-
-    SeedData.Initialize(services);
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new SpringContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<SpringContext>>()))
+        {
+            // Look for any Furnitures.
+            if (context.Furniture.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Furniture.AddRange(
+                new Furniture
+                {
+                    Name = "Super Seater",
+                    Colour = "Blue",
+                    Type = "Recliner",
+                    Price = 799M
+                },
+                new Furniture
+                {
+                    Name = "Super Sleeper ",
+                    Colour = "Beige",
+                    Type = "Mattress",
+                    Price = 899M
+                },
+                new Furniture
+                {
+                    Name = "Super Eater",
+                    Colour = "Oak",
+                    Type = "Table",
+                    Price = 999M
+                },
+                new Furniture
+                {
+                    Name = "Hide and Sleep",
+                    Colour = "Red",
+                    Type = "Sofa Bed",
+                    Price = 399M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
 }
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
