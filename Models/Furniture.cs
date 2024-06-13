1d08ad97212bea749c329efe278499446410c3c5
Matthew Gabrielle <mg244@myscc.ca>
2024-06-13 15:24:36 -0400

    Committ all

---
+++ Models/Furniture.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spring.Models;
+
+public class Furniture
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime Colour { get; set; }
+    public string? Type { get; set; }
+    public decimal Price { get; set; }
+}
